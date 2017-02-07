cfe24462fad6f6fabbff3fd22783ea738341f596
MichelFG <michelfigue@outlook.fr>
2017-02-07 12:39:19 +0100

    mise en forme

--- index.php
+++ index.php
@@ -4,8 +4,7 @@ require_once('./write_html.class.php');
 require_once('./functions.php');
 
 html_start("Page d'accueil",'./style.css');
-echo '<div class="CNX">Connexion';
-echo'<div class="CNX_Acc"><a href="connexion.php">Connexion</a></div>';
+echo'<div class="Lien_cnx"><a href="connexion.php">Connexion</a></div>';
 
 
 html_end();
