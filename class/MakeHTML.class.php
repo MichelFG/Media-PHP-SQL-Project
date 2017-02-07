867504f651432c261aacf536355c21ecd0d19fc9
MichelFG <michelfigue@outlook.fr>
2017-02-07 18:03:09 +0100

    changement nom fichier

--- class/MakeHTML.class.php
+++ class/MakeHTML.class.php
@@ -5,12 +5,15 @@ abstract class MakeHTML {
     static function html_start(string $title) {
     echo '<!DOCTYPE html>
         <html><head>'."\n";
+        
     echo '<title> $title </title>'."\n";
+        
         echo '<div class="header">'."\n";
         echo'<div class="Lien_cnx"><a href="connexion.php">Connexion</a></div>';
         echo '</div>';
+        
     echo '<meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0\">'."\n";
+            <meta name="viewport" content="width=device-width, initial-scale=1.0\">'."\n";
     echo '<link rel="stylesheet" href="./css/style.css">'."\n";
         
         echo '</head><body>'."\n";
