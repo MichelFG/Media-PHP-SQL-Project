5420c0ff21078b1dde34ae282635d3ab527ffb3c
MichelFG <michelfigue@outlook.fr>
2017-02-07 17:40:58 +0100

    changement de repertoire class

--- connexion.php
+++ connexion.php
@@ -1,11 +1,11 @@
 <?php
 
-require_once('./make_html.class.php');
-require_once('./functions.class.php');
-
+require_once 'class/autoload.php';
+    
 MakeHTML::html_start("Connexion");
 
 Form_GEN::formuGen();
+
 Form_GEN::formuGen();
 
 MakeHTML::html_end();
