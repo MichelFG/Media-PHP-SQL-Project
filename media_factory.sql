5df6ede1284a180eb54b55b710613ecd754bbadf
Ainsley Remy <ainsley.remy.pro@gmail.com>
2017-02-08 09:22:26 +0100

    bdd à tester Ajout de 2 colonnes suplémentaires

---
+++ media_factory.sql
@@ -0,0 +1,83 @@
+-- phpMyAdmin SQL Dump
+-- version 4.6.4
+-- https://www.phpmyadmin.net/
+--
+-- Client :  127.0.0.1
+-- Généré le :  Mar 07 Février 2017 à 16:31
+-- Version du serveur :  5.7.14
+-- Version de PHP :  5.6.25
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de données :  `media_factory`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `datas`
+--
+
+CREATE TABLE `datas` (
+  `id_auto` int(11) NOT NULL,
+  `auteur_id` int(30) NOT NULL,
+  `date` date NOT NULL,
+  `description` varchar(50) DEFAULT NULL,
+  `nom_du_fichier` varchar(30) NOT NULL,
+  `chemin_relatif` varchar(30) NOT NULL,
+  `mime_type` varchar(20) NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `user`
+--
+
+CREATE TABLE `user` (
+  `id_auto` int(11) NOT NULL,
+  `pseudo` varchar(30) NOT NULL,
+  `mdp` varchar(12) NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+--
+-- Index pour les tables exportées
+--
+
+--
+-- Index pour la table `datas`
+--
+ALTER TABLE `datas`
+  ADD PRIMARY KEY (`id_auto`);
+
+--
+-- Index pour la table `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`id_auto`);
+
+--
+-- AUTO_INCREMENT pour les tables exportées
+--
+
+--
+-- AUTO_INCREMENT pour la table `datas`
+--
+ALTER TABLE `datas`
+  MODIFY `id_auto` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT pour la table `user`
+--
+ALTER TABLE `user`
+  MODIFY `id_auto` int(11) NOT NULL AUTO_INCREMENT;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
