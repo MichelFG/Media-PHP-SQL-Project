a2c38485db9d2895a4396f9e9655297ff5fb43eb
Ainsley Remy <ainsley.remy.pro@gmail.com>
2017-02-09 10:59:48 +0100

    Test

--- connexion.php
+++ connexion.php
@@ -1,19 +1,10 @@
 <?php
-
 require_once 'class/autoload.php';
+$data=Session::getInstance();
+printf( '<p>My name is %s</p>', $data->pseudo );
     
 MakeHTML::html_start("Connexion");
 
-echo '<div class="Form">';
-
-echo '<h2>Connectez vous</h2>';
-
 Form_GEN::formuGen();
-
-echo '<h2>Inscrivez vous</h2>';
-
-echo'<div class ="Lien_cnx"><a href="inscription.php">Inscription</a></div>';
-
-echo '</div>';
 
 MakeHTML::html_end();
