f373caaf9316e18d79e8b2939b9eadaf84483c3a
Ainsley Remy <ainsley.remy.pro@gmail.com>
2017-02-09 09:51:51 +0100

    Modif de la syntaxe

--- class/Form_GEN.class.php
+++ class/Form_GEN.class.php
@@ -3,12 +3,9 @@
 
 class Form_GEN
 {
-    
     static function formuGen() 
     {
-      
-        
-        echo '<form method=POST action=".//.php">';
+        echo '<form name="login" method="post" action="#">';
         echo '<input type="text" class="pswd" name="nom" placeholder="Votre nom">';
         echo '<br>';
         echo '<input type="password" class="pswd" name="password" placeholder="Votre mot de passe">';
