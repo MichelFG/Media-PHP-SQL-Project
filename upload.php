cd521641e0561d9710813e8f703c64dd8b5ca7f6
Tfdi <tfdi@wanadoo.fr>
2017-02-09 14:02:47 +0100

    update upload

--- upload.php
+++ upload.php
@@ -2,24 +2,28 @@
 require_once 'class/autoload.php';
     
 MakeHTML::html_start("Connexion");
-      ?>
-    <div class="form_upload">
-        <h2>Envoyer des fichiers</h2>
-
-        <form action="#" method="post" enctype="multipart/form-data" id="upload">
-
-            <div class="upload_form">
-                <input type="hidden" name="MAX_FILE_SIZE" value="100000" />
-                <input type="file" name="files" id="file" multiple="multiple" title="Message d'erreur" />
-                <div class="txtarea">
-                    Description du fichier :
-                    <br>
-                    <textarea cols="30" rows="4" name="txtarea"></textarea>
-                </div>
-            </div>
-            <input type="submit" class="Lien_cnx" name="submit" value="Envoyer" />
-
-        </form>
-    </div>
-    <?php
+	 if(isset($_GET['valid']))
+	 {
+		 // Ici on fait les test de validation avec la récup des valeurs sur les champs du formulaire
+		 //
+		 
+		 
+		 
+	 }
+	 else
+	 {
+			 echo '<div class="form_upload">';
+			 echo '<h2>Envoyer des fichiers</h2>';
+			 echo '<form action="upload.php?valid" method="post" enctype="multipart/form-data" id="upload">';
+			 echo '<div class="upload_form">';
+			 echo '<input type="hidden" name="MAX_FILE_SIZE" value="100000" />';
+			 echo '<input type="file" name="files" id="file" title="Message d\'erreur" />';
+			 echo '<div class="txtarea">';
+			 echo 'Description du fichier :<br>';
+			 echo '<textarea cols="30" rows="4" name="txtarea"></textarea>';
+			 echo '</div></div>';
+			 echo '<input type="submit" class="Lien_cnx" name="submit" value="Envoyer" />';
+			 echo '</form>';
+			 echo '</div>';
+	 }
 MakeHTML::html_end();
