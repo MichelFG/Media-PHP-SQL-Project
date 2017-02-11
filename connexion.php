7a49445c89844ea1f34817a57b497a1c5a47a780
Ainsley Remy <ainsley.remy.pro@gmail.com>
2017-02-11 16:01:34 +0100

    - html vérifié - modif recherche (ajout label) - resol pb d'alignements et de balises

--- connexion.php
+++ connexion.php
@@ -1,10 +1,8 @@
 <?php
 require_once 'class/autoload.php';
 $data=Session::getInstance();
-printf( '<p>My name is %s</p>', $data->pseudo );
-    
 MakeHTML::html_start("Connexion");
-
+printf( '<p>My name is %s</p>', $data->pseudo );
 echo '<div class="Form">';
 
 echo '<h2>Connectez vous</h2>';
@@ -12,5 +10,8 @@ echo '<h2>Connectez vous</h2>';
 Form_GEN::formuGen();
 
  echo'<div class="Lien_cnx"><a href="inscription.php">Inscription</a></div>';
+ 
+ echo '</div>';
+
 
 MakeHTML::html_end();
